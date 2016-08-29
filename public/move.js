72208a307c1cc514459c8d74a6fc10e3a0e07555
Salomon <salomonpluviose@yahoo.com>
2016-08-29 16:04:55 -0400

    redid whole page, added parallaxes to make beautiful

---
+++ public/move.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  console.log('working');
+  $("#css").hover(function(){
+    console.log('hovered');
+    // $('#html').effect('shake', { direction: "up", times: 4, distance: 10}, 1000);
+  });
+});
